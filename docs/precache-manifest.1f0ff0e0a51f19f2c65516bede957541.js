6134df83897127cbfb360b79e9d085fe3dce823c
sorabeast <sorabeast@beast.com>
2020-03-30 12:33:29 -1000

    v1 - build

---
+++ docs/precache-manifest.1f0ff0e0a51f19f2c65516bede957541.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0064126afeee876c022fa283299620b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65a9bb5afae1bc736784",
+    "url": "/static/css/main.167fc0d5.chunk.css"
+  },
+  {
+    "revision": "d2ec78e5a814348a8762",
+    "url": "/static/js/2.c5ee0ff1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c5ee0ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65a9bb5afae1bc736784",
+    "url": "/static/js/main.77b5d2cb.chunk.js"
+  },
+  {
+    "revision": "ce717acb90340db90d40",
+    "url": "/static/js/runtime-main.16fff560.js"
+  }
+]);
